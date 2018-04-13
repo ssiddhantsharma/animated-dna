f70badad34b90250e4607c9a426449d740499edc
Siddhant Sharma <siddhaantsharma.ss@gmail.com>
2018-04-13 16:23:15 +0530

    Update three.js

--- three.js
+++ three.js
@@ -1,5 +1,5 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(95, window.innerWidth / window.innerHeight, 0.01, 1000);
+var camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.01, 1000);
 
 var renderer = Detector.webgl ? new THREE.WebGLRenderer({
     antialias: true
@@ -7,12 +7,12 @@ var renderer = Detector.webgl ? new THREE.WebGLRenderer({
 
 var blue = 0xC70039;
 var yellow = 0x0c25d8;
-var purple = 0x000000;
+var purple = 0xFFFFFF;
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-camera.position.z = 48;
+camera.position.z = 50;
 
 var tubeGeometry = new THREE.CylinderGeometry(0.3, 0.3, 6, 32);
 var ballGeometry = new THREE.SphereGeometry(0.8, 32, 32);
@@ -30,7 +30,7 @@ var dna = new THREE.Object3D();
 var holder = new THREE.Object3D();
 
 
-for (var i = 0; i <= 35; i++) {
+for (var i = 0; i <= 33; i++) {
     var blueTube = new THREE.Mesh(tubeGeometry, blueMaterial);
     blueTube.rotation.z = 90 * Math.PI / 180;
     blueTube.position.x = -2;
